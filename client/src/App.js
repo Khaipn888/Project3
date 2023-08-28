65480b8dec983b50e9189d0e8b3ffaf2c668d9b2
Your Name <you@example.com>
2023-08-28 15:02:48 +0700

    init project

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import './index.css'
+function App() {
+  return (
+    <div className="text-center	">
+      <h1 className="text-rose-800 text-7xl">My Web</h1>
+    </div>
+  );
+}
+
+export default App;
