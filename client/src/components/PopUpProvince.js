e099a53acc303b4a68544b07ea46e3c969b2522b
Your Name <you@example.com>
2023-09-03 16:48:19 +0700

    create PopUpProvince

---
+++ client/src/components/PopUpProvince.js
@@ -0,0 +1,13 @@
+
+
+function PopUpProvince() {
+    return (
+        <div className="pop-up-container w-full h-[100vh] relative bg-gray-100">
+            <div className="pop-up-box"></div>
+
+        </div>
+
+    )
+}
+
+export default PopUpProvince;
